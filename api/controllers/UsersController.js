8a431e2a3c50286192a845cc5a64fbf775cf49c2
Camilo Ramirez <jucarave92@hotmail.com>
2017-09-04 12:42:21 -0500

    Testing bcrypt

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -5,7 +5,14 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var bcrypt = require('bcryptjs');
+
 module.exports = {
-	
+	login: function(req, res) {
+        var salt = bcrypt.genSaltSync(10),
+            hash = bcrypt.hashSync("myPassword", salt);
+
+        res.send(bcrypt.compareSync("myPassword", hash));
+    }
 };
 
--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,9 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
-  }
+  },
+  
+  'POST /api/v1/users/login':  "UsersController.login"
 
   /***************************************************************************
   *                                                                          *
